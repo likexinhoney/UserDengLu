c7df2992db243700b91037834914cef243832d59
李可鑫 <2311710315@qq.com>
2019-05-12 17:13:22 +0800

    123

---
+++ src/Dengluservlet.java
@@ -0,0 +1,86 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.ResultSet;
+import com.mysql.jdbc.Statement;
+
+
+public class Dengluservlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();//设置一个输出的为out的方法
+		String username = request.getParameter("username");
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = (Connection) DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/userdb", "root", "admin");//连接数据库
+			Statement sta = (Statement) con.createStatement();
+			ResultSet rs = (ResultSet) sta.executeQuery("SELECT user from user WHERE user='"+ username + "'");
+			if (rs.next()) {
+				request.getRequestDispatcher("/servlet/Successservlet").forward(request, response);
+			} else {
+				response.sendRedirect("/UserDengLu/Zhuce.html");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			out.print("没找到");
+		} catch (SQLException sqle) {
+
+			out.print("连接异常");
+		}
+	}
+
+
+	
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the POST method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+}
